2471450d6723f0087f68b78f7ae7b3d165233296
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-08-01 18:04:55 +0300

    quaternion estimation using EKF

--- EKFq.m
+++ EKFq.m
@@ -6,12 +6,8 @@
 global glv;
 N=length(imu.gyros);
 
-%% zero velociy detection
-% [zvd,~] = zeroVelocityDetector(imu,config);
-[gait_time,~,~,zvd,~,~] = gait_devide_ZVD_MXFforAtt(imu,glv.g0);
-
 %% initial
-% ### done in the main test file.
+% ########### done in the main test file ###########
 
 %% Filter   
 MM = zeros(size(m,1),N);
@@ -19,22 +15,40 @@ PP = zeros(size(P,1),size(P,2),N);
  
 for k=1:N
     
-    % ### prediction via dynamics
+    % ########### prediction via dynamics ###########
     [f,F] = f_qua(X_type,m,[imu.gyros(k,:),imu.ts],'Eular');
 
     m = f;
-    P = F*P*F' + Q;
+%     G = imu.ts/2*[-m(2), -m(3) -m(4);m(1) -m(4) m(3);m(4) m(1) -m(2);-m(3) m(2) m(1)];
+%     Q = diag([1e-12*0.02;1e-12*0.02;1e-12*0.02]);
+%     P = F*P*F' + G*Q*G';
+    P = F*P*F';
     m(1:4) = m(1:4)/norm(m(1:4));
     
-    % ### measuremnet update
+    % ########### measuremnet update ###########
     if zvd(k)==1 
-        [z_pre,z_obs,H] = h(X_type,m,obs_type,obs(k,:)',para);
+        [z_pre,z_obs,H] = h_meas(X_type,m,obs_type,obs(k,:)',para);
         S = H*P*H' + R;
         K = P*H'/S;
         m = m+ K*(z_obs - z_pre);
         P = P - K*S*K';
         m(1:4) = m(1:4)/norm(m(1:4));
     end       
+%     if (k<783 || (k>1052 && k<1497) || k>1690) && (zvd(k)==1)
+% %         [z_pre,z_obs,H] = h(X_type,m,obs_type,obs(k,:)',para);
+%         z_obs = qua_ref(k,:)';
+%         z_pre = m(1:4);
+%         H = eye(4);
+%         R = diag([1e-10;1e-10;1e-10;1e-10]);
+%         S = H*P*H' + R;
+%         K = P*H'/S;
+%         m = m+ K*(z_obs - z_pre);
+%         P = P - K*S*K';
+%         m(1:4) = m(1:4)/norm(m(1:4));
+%     end  
+%     if (k<783 || (k>1052 && k<1497) || k>1690) && (zvd(k)==1)
+%         m(1:4) = qua_ref(k,:)';
+%     end  
     P = (P + P')/2;    
     
     MM(:,k) = m;
@@ -63,11 +77,15 @@ if strcmp(X_type,'q bG')
     figure
     plot(t/60,bG*glv.deg)
     title('Gyroscope bias');
-    if strcmp(X_type,'q bG bA')
+end
+if strcmp(X_type,'q bG bA')
+        bG = MM(5:7,:)';
+        figure
+        plot(t/60,bG*glv.deg)
+        title('Gyroscope bias');
+        
         bA = MM(8:10,:)';
         figure
         plot(t/60,bA)
         title('Accelerometer bias');
-    end
-else
 end
