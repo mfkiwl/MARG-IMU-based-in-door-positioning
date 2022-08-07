533412ee9c51d35f4763944c11e8614c3a9fd1f2
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-08-07 14:31:33 +0300

    gait dividing after zero velocity detecting

--- zeroVelocityDetection/gaitDivide.m
+++ zeroVelocityDetection/gaitDivide.m
@@ -1,4 +1,4 @@
-function [arr_gait_time,static_start_index,static_end_index,zvi,Test_statistics]=gaitDivide(imu,g)
+function [arr_gait_time,static_start_index,static_end_index,zvi,Test_statistics]=gaitDivide(imu,g,plot_flag)
 
 % ############ zero velocity interval detecting ############
 config.detector_type = 'GLRT';%'MAG';% 'MV','GLRT'
@@ -18,13 +18,13 @@ static_end_index = find([0; diff(zvi)] == -1); % 1 to 0, i.e. static to move
 % ############ Removing very short opposite intervals ############
 % If an swing phase is too short, it is classified as a stance phase
 n=length(static_start_index);
-% for i=2:n
-%     if(stationaryEnd(i,1) -stationaryStart(i-1,1)< 5)
-%         stationary(stationaryStart(i-1,1):stationaryEnd(i,1),1)=0;
-%     end
-% end
+for i=2:n
+    if(static_end_index(i,1) - static_start_index(i-1,1)< 5)
+        zvi(static_start_index(i-1,1):static_end_index(i,1),1)=0;
+    end
+end
 for i=1:n 
-    if(static_start_index(i,1)-static_end_index(i,1) < 5)
+    if(static_start_index(i,1) - static_end_index(i,1) < 5)
         zvi(static_end_index(i,1):static_start_index(i,1),1)=1;
     end
 end
@@ -34,8 +34,10 @@ static_end_index = find([0; diff(zvi)] == -1);
 
 arr_gait_time=[static_end_index,static_start_index]';
 
+if strcmp(plot_flag,'fig')
 figure;hold on
 plot(imu.gyros(:,1));
 plot(zvi)
+end
 
 end
