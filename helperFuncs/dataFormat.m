4ca90edd00b4da6a1faa8ba43761d9ab13c9787f
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-08-08 14:51:06 +0300

    format the data in a structure

--- helperFuncs/dataFormat.m
+++ helperFuncs/dataFormat.m
@@ -23,7 +23,10 @@ switch product
         imu.sT = file(:,2);
         imu.acc = (sSys2rfu('flu')*file(:,3:5)')'; % m/s^2 converted to m/s^2
         imu.gyros = (sSys2rfu('flu')*file(:,6:8)')' * glv.rad; % deg/s converted to rad/s
-        imu.mag = (sSys2rfu('flu')*file(:,9:11)')'*49*1000; % a.u. converted to nT
+        imu.mag = (sSys2rfu('flu')*file(:,9:11)')'*49*1000; % a.u. converted to nT 
+        % for mag, 49 (at XSens factory) should be changed to local EMF at
+        % where the new MFM was done, but the unit is irrelevant when
+        % calculating the yaw
         imu.ts = 1/60;
     case 'Xsens_custom4' % Custome mode 4 
         imu.sT = file(:,2);
