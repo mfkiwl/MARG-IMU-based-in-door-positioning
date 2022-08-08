48b909ce744c306b83526a65dbfbb17967cb80bd
Xiaofeng Ma <i.xiaofeng.ma@outlook.com>
2022-08-08 10:29:00 +0300

    EKF for quaternion

--- EKFq.m
+++ EKFq.m
@@ -16,14 +16,14 @@ PP = zeros(size(P,1),size(P,2),N);
 for k=1:N
     
     % ########### prediction via dynamics ###########
-    [f,F] = f_qua(X_type,m,[imu.gyros(k,:),imu.ts],'Eular');
+    [f,F] = f_qua(X_type,m,[imu.gyros(k,:),imu.ts],'exp');
 
     m = f;
-%     G = imu.ts/2*[-m(2), -m(3) -m(4);m(1) -m(4) m(3);m(4) m(1) -m(2);-m(3) m(2) m(1)];
-%     Q = diag([1e-12*0.02;1e-12*0.02;1e-12*0.02]);
-%     P = F*P*F' + G*Q*G';
-    P = F*P*F';
-    m(1:4) = m(1:4)/norm(m(1:4));
+    G = imu.ts/2*[-m(2), -m(3) -m(4);m(1) -m(4) m(3);m(4) m(1) -m(2);-m(3) m(2) m(1)];
+    Q = diag([10;10;10]*glv.rad/3600);
+    P = F*P*F' + G*Q*G';
+%     P = F*P*F'+Q;
+%     m(1:4) = m(1:4)/norm(m(1:4));
     
     % ########### measuremnet update ###########
     if zvd(k)==1 
@@ -32,8 +32,13 @@ for k=1:N
         K = P*H'/S;
         m = m+ K*(z_obs - z_pre);
         P = P - K*S*K';
-        m(1:4) = m(1:4)/norm(m(1:4));
-    end       
+% att_g = qua2att(m,order);
+% [~,att_m] = alignbyAccMagatStatic(imu.acc(k,:),imu.mag(k,:),order);
+% m = att2qua([att_g(1:2),att_m(3)],order);
+    end  
+
+    m(1:4) = m(1:4)/norm(m(1:4));
+
 %     if (k<783 || (k>1052 && k<1497) || k>1690) && (zvd(k)==1)
 % %         [z_pre,z_obs,H] = h(X_type,m,obs_type,obs(k,:)',para);
 %         z_obs = qua_ref(k,:)';
@@ -58,17 +63,17 @@ end
 t = 0:imu.ts:imu.ts*(N-1);
 Ref = zeros(5,N) + [180;90;0;-90;-180];
     
-figure
-plot(t,MM,'Linewidth',1);
-title('EKF estimate');
+% figure
+% plot(t,MM,'Linewidth',1);
+% title('EKF estimate');
 
 att_EKF = qua2attV(MM', 'zxy');
-figure
-plot(t/60,att_EKF*glv.deg,'Linewidth',1);
-hold on;
-plot(t/60,Ref,'r:','Linewidth',1.5)
-legend('pitch','roll','yaw','reference');
-title('EKF attitude estimates');
+% figure
+% plot(t/60,att_EKF*glv.deg,'Linewidth',1);
+% hold on;
+% plot(t/60,Ref,'r:','Linewidth',1.5)
+% legend('pitch','roll','yaw','reference');
+% title('EKF attitude estimates');
 
 q = MM(1:4,:)';
 
